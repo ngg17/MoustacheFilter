33f589b66e886e8955f44e9394f9b8ea62b4f282
ngg17 <71960903+ngg17@users.noreply.github.com>
2020-10-24 11:56:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-function preload() {
+mouthX=0;
+mouthY=0;
 
+function preload() {
+mouth = loadImage('https://i.postimg.cc/zff0M5bP/Moustache.png');
 }
 
 function setup() {
     canvas = createCanvas(300, 300);
-    canvas.center();
+    canvas.position(530, 200);
     video = createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
@@ -22,13 +25,16 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        mouthX = results[0].pose.nose.x - 20;
+        mouthY = results[0].pose.nose.y;
+        console.log("mouth x = " + mouthX);
+        console.log("mouth y = " + mouthY);
     }
 }
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(mouth, mouthX, mouthY, 50, 30);
 }
 
 function take_snapshot() {
